68e8f5999319f7707eecd145fd41334cb4243656
exalted100 <harounayomikun@gmail.com>
2022-09-23 00:33:56 +0100

    feat: adjusted score schema

--- models/entity/score.go
+++ models/entity/score.go
@@ -2,7 +2,7 @@ package entity
 
 //User struct is to handle user data
 type Score struct {
-	User  string `json:"user" bson:"user"`
+	User  string `json:"user" bson:"user" binding:"required"`
 	Mode  string `json:"mode" bson:"mode" binding:"required"`
 	Score float64 `json:"score" bson:"score" binding:"required"`
 }
